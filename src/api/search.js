f380ff3a6e1df9ecbbb047f0164b8b5621584712
gokceyucel <gokce.yucel@emlakjet.com>
2018-09-24 16:43:40 +0300

    Fetch 20 movies at once

--- src/api/search.js
+++ src/api/search.js
@@ -10,20 +10,38 @@ export default ({ config, db }) => {
 
 	search.get('/', async (req, res) => {
 
-		const getMovies = async url => {
+		// TODO: move this elsewhere. Maybe to a service.
+		const getMovies = async (keyword, page) => {
 			try {
+				// Get this url from config along with api key.
+				const pageNumber = page || 1;
+				const url = `http://www.omdbapi.com/?apikey=3f1022a5&s=${keyword}&page=${pageNumber}`;
 				const response = await axios.get(url);
 				const data = response.data;
 				return data;
 			} catch (error) {
-				console.log(error);
+				// TODO: do a better error handling. Not returning any response in case of an error.
+				console.error(error);
 			}
-		};
+		}
+
+		const get20movies = async keyword => {
+			try {
+				const movies = await getMovies(keyword);
+				const totalMoviesCount = movies.totalResults;
+				if (totalMoviesCount > 10) {
+					const moviesPage2 = await getMovies(keyword, 2);
+					const movies20 = [...movies.Search, ...moviesPage2.Search];
+					return movies20;
+				}
+			} catch(error) {
+				console.error(error);
+			}
+		}
 
 		try {
 			const keyword = req.query.keyword;
-			const url = `http://www.omdbapi.com/?apikey=3f1022a5&s=${keyword}&page=1`;
-			const response = await getMovies(url);
+			const response = await get20movies(keyword);
 			res.json(response);
 		} catch (error) {
 			res.json("error occured");
